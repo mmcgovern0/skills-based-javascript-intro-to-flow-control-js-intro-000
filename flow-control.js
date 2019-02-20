2a71bc85502f7a36382626a190d2e03f17599182
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-20 20:53:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,17 @@
-function basicTeenager(age) {
+function isTeenager(age) {
+  return age >= 13 && age <= 19;
+}
 
+function basicTeenager(age) {
+  if (isTeenager(age)) return "You are a teenager!";
 }
 
 function teenager(age) {
-
+  if (isTeenager(age)) {
+    return "You are a teenager!";
+  } esle {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
